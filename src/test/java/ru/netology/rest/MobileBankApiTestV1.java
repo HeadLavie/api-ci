47b0447ec240edd17320897159ae9fedb75c8fef
lavie head <nadya.karpenko@gmail.com>
2023-12-09 20:48:52 +0000

    Final commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -1,24 +1,29 @@
 package ru.netology.rest;
 
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.either;
+import static org.hamcrest.Matchers.equalTo;
 
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-      // Given - When - Then
-      // Предусловия
-      given()
-          .baseUri("http://localhost:9999/api/v1")
-      // Выполняемые действия
-      .when()
-          .get("/demo/accounts")
-      // Проверки
-              .then()
-              .statusCode(200)
-              .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                .body("[0].currency", equalTo("USD"))
+                .body("[0].currency", either(equalTo("RUB")).or(equalTo("USD")));
     }
 }
-// тест прошел
